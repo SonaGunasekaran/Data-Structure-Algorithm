e57b68a4b870a320904ccf48065b47189175f14a
[Sona] <[sona16061999@gmail.com]>
2021-07-19 12:00:25 +0530

    Initialised with Welcome message

---
+++ DataStructureAlgorithms/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataStructureAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Data Structures Algorithm Programs!");
+        }
+    }
+}
