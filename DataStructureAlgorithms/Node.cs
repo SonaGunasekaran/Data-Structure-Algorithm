66e47d47b82579a9d36a6ec8818e133adf179b8e
[Sona] <[sona16061999@gmail.com]>
2021-07-20 22:24:42 +0530

    [Sona] Check whether the string is palindrome or not using queue

--- DataStructureAlgorithms/Node.cs
+++ DataStructureAlgorithms/Node.cs
@@ -8,10 +8,13 @@ namespace DataStructureAlgorithms
     {
         public T value;
         public Node<T> Next;
+        public Node<T> prev;
+
         public Node(T value)
         {
             this.value = value;
             this.Next = null;
+            this.prev = null;
         }
     }
 }
